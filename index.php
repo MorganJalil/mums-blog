18032fe3ce364934600c4a8fac62dbfba10de8d5
Michael Persson <michaelpersson@MikeBook-Pro.mediegruppen.se>
2018-11-23 11:22:28 +0100

    include bootstrap js

--- index.php
+++ index.php
@@ -7,14 +7,15 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-
     <title>Document</title>
 </head>
 <body>
     
 
 
+<?php 
+    include 'includes/bootstrap_js.php';
+?>
 
-    
 </body>
 </html>
