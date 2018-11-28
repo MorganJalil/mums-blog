3a6fd4d181b5fd36a1b34385462f4a6501c39559
Michael Persson <michaelpersson@MikeBook-Pro.local>
2018-11-28 11:04:10 +0100

    add single post structure

--- views/single_post.php
+++ views/single_post.php
@@ -1,7 +1,9 @@
 <?php
 
+//Start session for comment and admin 
 session_start();
 
+//get database connection once
 require_once '../includes/database_connection.php';
 
 ?>
@@ -17,7 +19,18 @@ require_once '../includes/database_connection.php';
 </head>
 <body>
     
-
+    <!--Display post image-->
+    <img src="" alt="Cool Post Image">
+    <!--Display post title-->
+    <h1>Title</h1>
+    <!--Display post category-->
+    <h2>Category</h2>
+    <!--Display post date-->
+    <h3>Date</h3>
+    <!--Display post -->
+    <p>Random post text</p>
+    <!--Display comments with comment field and if admin display remove comment button-->
+    <section class="comments">Comments</section>
 
 <?php 
     include '../includes/bootstrap_js.php';
