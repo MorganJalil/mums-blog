1daa8fc10081a6e2b9470f3b50a03057c0834688
Morgan J <Moggelicious@hotmail.com>
2018-12-01 11:16:29 +0100

    added backend checks for mail and username

--- classes/User.php
+++ classes/User.php
@@ -2,21 +2,43 @@
 include '../includes/functions.php';
 
 class User {
-        
     private $pdo;
     public function __construct($pdo) {
         $this->pdo = $pdo;
     }
 
+    // Method to sanitize variable from scripts and more.
+    private function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
     //method to insert user data into db
     public function register($username, $password, $email) {
 
-        if (!empty($username) && !empty($email)) {
+        if(strlen($username) < 4 || ctype_space($username)){
+            header('Location:../views/register_user.php?=namefailed');
+            exit();
+        } else {
+            $username = test_input($username);
+    }   
 
-            $username = trim(htmlspecialchars($username));
-            $email = (htmlspecialchars($email));
-        
+    
+    if(strlen($password) < 4 || ctype_space($password)){
+        header('Location:../views/register_user.php?=passfailed');
+        exit();
+    } 
+    
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo("$email is a valid email address");
+        } else {
+            header('Location:../views/register_user.php?=novalidmailfailed');
+            exit();
         }
+    
 
 
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
