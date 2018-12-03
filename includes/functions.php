5c333afcfd44954574b1c7ecb5f15626ce49c303
Michael Persson <michaelpersson@MikeBook-Pro.mediegruppen.se>
2018-12-03 14:00:24 +0100

    fix single post function

--- includes/functions.php
+++ includes/functions.php
@@ -1,20 +1,35 @@
 <?php
 
 function getSinglePost($pdo) {
-   
-    $single_post = $pdo->prepare('SELECT * FROM posts WHERE id = ');
+    $post_id = 1;
+    $id= 1;
+   //kolla om man kan ersätta 1 med ?
+    $single_post = $pdo->prepare('SELECT * FROM posts WHERE id = :id');
 
-    $post_id = 
+    $single_post->execute([
+
+        'id' => $post_id,
     
-    return $single_post;
+    ]);
+    
+    $fetched_post = $single_post->fetch();
+
+   if (isset($fetched_post["id"])) {
+    var_dump($fetched_post["id"]);
+   } else {
+     header('location: ../index.php?=kebabfail');
+   }
+   
+    
+    
 
 }
 
-function getSinglePostCategory($pdo) {
+//function getSinglePostCategory($pdo) {
    
-    $single_post = $pdo->prepare('SELECT category_id FROM post_category WHERE post_id = ');
+    //$single_post = $pdo->prepare('SELECT category_id FROM post_category WHERE post_id = ');
 
     
-    return $single_post;
+     //$single_post;
 
-}
+//}
