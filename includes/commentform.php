cc1c152b6ddc6ee1c1527bb61502d349fcafb8f0
Michael Persson <michaelpersson@MikeBook-Pro.mediegruppen.se>
2018-12-04 15:11:12 +0100

    clear up database insertion

--- includes/commentform.php
+++ includes/commentform.php
@@ -2,11 +2,9 @@
 require ('database_connection.php');
 
 if(isset($_POST) & !empty($_POST)){
-	$name = mysqli_real_escape_string($connection, $_POST['name']);
-	$email = mysqli_real_escape_string($connection, $_POST['email']);
 	$subject = mysqli_real_escape_string($connection, $_POST['subject']);
 
-	$isql = "INSERT INTO comments (name, email, subject, status) VALUES ('$name', '$email', '$subject', 'draft')";
+	$isql = "INSERT INTO comments (subject) VALUES ('$subject')";
 	$ires = mysqli_query($connection, $isql) or die(mysqli_error($connection));
 	if($ires){
 		$smsg = "Your Comment Submitted Successfully";
