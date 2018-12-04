0122ffee14f3cc6aee96ec63d3ac18a841f13859
Morgan J <Moggelicious@hotmail.com>
2018-12-04 22:49:53 +0100

    index.php fixings

--- index.php
+++ index.php
@@ -1,54 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/normalize.css">
-    <title>Document</title>
-</head>
-<body>
-
-<a href="views/register_user.php">Register</a>
-
 <?php
 session_start();
-include 'includes/bootstrap_js.php';
-include 'includes/database_connection.php';
+include 'includes/header.php';
+?>
 
-if (isset($_SESSION['user_id'])) {
+<body style="color: blue;">
+    <header role="banner">
+        <img src="images/logo_borders.png" class="hero_image" alt="Logo Big">
+        <nav role="navigation">
 
-    $request = $pdo->prepare('SELECT users.username, posts.image, posts.created_at, posts.title, posts.body FROM posts INNER JOIN users ON posts.created_by = users.user_id AND users.admin = 1 AND admin = 1; ');
+        </nav>
+    </header>
 
-    $request->execute();
+    <div class="wrap">
+        <main role="main">
 
-    $posts = $request->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach ($posts as $post): ?>
-
-    <?= $post['image'] ?>
-    <?= $post['title'] ?>
-    <?= $post['username'] ?>
-    <?= $post['created_at'] ?>
-    <?= $post['body'] ?>
-    <?php endforeach; ?>
-
-    <button><a class="logoutButton" href="views/logout.php">LOGGAUT</a></button>
-
-
-<?php } else { ?>
-
-    <form action="views/login.php" method="post">
+        <form action="views/login.php" method="post">
         <input type="text" name="username">
         <input type="password" name="password">
         <button type="submit" name="login"> Log in
         </button>
     </form>
+        <a href="views/register_user.php">Register</a>
+        </main>
+    </div>
 
-<?php } ?>
-
+    <footer role="contentinfo">
+        <address>
+          <p>For further information, please contact <a href="mailto:admin@example.com">Millhouse</a>.</p>
+        </address>
+        <small>Copyright &copy; <time>2018</time></small>
+    </footer>
 
 </body>
 </html>
