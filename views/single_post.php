ecb2783b33d7c78314a3d5f392da5c1817237df2
Michael Persson <michaelpersson@MikeBook-Pro.mediegruppen.se>
2018-12-05 15:57:46 +0100

    remove error message

--- views/single_post.php
+++ views/single_post.php
@@ -44,8 +44,6 @@ require_once '../includes/functions.php';
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-            <?php if(isset($smsg)){ ?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div><?php } ?>
-            <?php if(isset($fmsg)){ ?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div><?php } ?>
         </div>
     </div>
 
