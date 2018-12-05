59d469e4bf12f1af98027acc15f010f3d36c01d2
SherieBach <Sherie.klappenbach@gmail.com>
2018-12-05 11:35:13 +0100

    added photos

--- views/main_page.php
+++ views/main_page.php
@@ -9,18 +9,15 @@
     <title>Millhouse</title>
 </head>
 <body id="main-page">
-<div class="grid-container">
+<?php
+include '../includes/bootstrap_js.php';
+?>
+<nav class="navbar sticky-top">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <input class="btn btn-primary" type="button" name="login" value="Login">
 
-    <div class="grid-item">1</div>
-    <div class="grid-item">2</div>
-    <div class="grid-item">3</div>
-    <div class="grid-item">4</div>
-    <div class="grid-item">5</div>
-    <div class="grid-item">6</div>
-    <div class="grid-item">7</div>
-    <div class="grid-item">8</div>
-    <div class="grid-item">9</div>
 
-</div>
+</nav>
+
 </body>
 </html>
