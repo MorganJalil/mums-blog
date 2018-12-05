cb6f0051cede441c96a1a6883b74e54264efa992
Michael Persson <michaelpersson@MikeBook-Pro.mediegruppen.se>
2018-12-05 14:44:18 +0100

    fix post action

--- views/single_post.php
+++ views/single_post.php
@@ -37,10 +37,10 @@ require_once '../includes/functions.php';
     <div class="panel panel-default">
         <div class="panel-heading">Submit Your Comments</div>
         <div class="panel-body">
-            <form method="post">
+            <form method="post" action="../includes/commentform.php"> 
             <div class="form-group">
-                <label for="exampleInputPassword1">Subject</label>
-                <textarea name="subject" class="form-control" rows="3"></textarea>
+                <label for="single_comment">Comment</label>
+                <textarea id="single_comment" name="content" class="form-control" rows="3"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
             </form>
