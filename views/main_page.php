7c6e3e92315f99af60a766477eda82713e05cda4
SherieBach <Sherie.klappenbach@gmail.com>
2018-12-07 17:02:14 +0100

    html col/rows adjustment

--- views/main_page.php
+++ views/main_page.php
@@ -14,7 +14,16 @@
 <body id="main-page">
 <?php
 include '../includes/bootstrap_js.php';
-?>
+include '../includes/database_connection.php';
+
+/*
+    $request = $pdo->prepare('SELECT users.username, posts.image, posts.created_at, posts.title, posts.body FROM posts INNER JOIN users ON posts.created_by = users.user_id AND users.admin = 1 AND admin = 1; ');
+
+    $request->execute();
+
+    $posts = $request->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($posts as $post): */ ?>
 
 <!-- N A V . B A R -->
 <nav class="navbar navbar-default navbar-expand-lg navbar-light bg-light">
@@ -53,56 +62,65 @@ include '../includes/bootstrap_js.php';
 </header>
 
 <!--- M A I N CONTENT --->
+<!---->
+<!---->
+<? //= $post['image'] ?>
+<? //= $post['title'] ?>
+<? //= $post['username'] ?>
+<? //= $post['created_at'] ?>
+<? //= $post['body'] ?>
+<?php //endforeach; ?>
 
 <main class="container post_section ">
     <section>
         <div class="row latest_featured_article">
-            <div class="col-12 post_parallax">
-                <a href="">
-                <img src="../images/paint_featured_interior%20copy.jpg">
-                </a>
+            <div class="col-6 post_parallax">
+                <a href=""><img src="../images/paint_featured_interior%20copy.jpg"></a>
                 <div class="text-block">
                     <h4>Article</h4>
                     <p>Poop</p>
                 </div>
             </div>
-            <div class="col-6 post_parallax">
-                <a href="">
-                <img src="../images/jen-p-541467-unsplash%20copy.jpg">
-                </a>
+            <div class="col-4 post_parallax">
+                <a href=""><img src="../images/jen-p-541467-unsplash%20copy.jpg"></a>
                 <div class="text-block">
                     <h4>Article</h4>
                     <p>Poop </p>
                 </div>
             </div>
-            <div class="col-6 post_parallax">
-                <a href="">
-                <img src="../images/dane-deaner-272375-unsplash%20copy.jpg">
-                </a>
-                <div class="text-block">
-                    <h4>Article</h4>
-                    <p>Poop</p>
+            <div class="row latest_featured_article">
+                <div class="col-10 post_parallax">
+                    <a href=""> <img src="../images/tamara-bellis-388531-unsplash.jpg"></a>
+                    <div class="text-block">
+                        <h4>Article</h4>
+                        <p>Poop </p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="row featured_articles">
-            <div class="col-sm">
-                <a href="">
-                <img src="../images/julian-o-hayon-237297-unsplash%20copy.jpg">
-                </a>
+            <div class="col-5">
+                <a href=""><img src="../images/stil-243522-unsplash.jpg"></a>
                 <div class="text-block">
                     <h4>Article</h4>
                     <p>Poop</p>
                 </div>
             </div>
-            <div class="col-sm">
-                <a href="">
-                <img src="../images/alexandru-acea-674023-unsplash.jpg">
-                </a>
+            <div class="col-5 post_parallax">
+                <a href=""><img src="../images/dane-deaner-272375-unsplash%20copy.jpg"></a>
                 <div class="text-block">
                     <h4>Article</h4>
                     <p>Poop</p>
                 </div>
+            </div>
+            <div class="row featured_articles">
+            <div class="col-10 post_parallax">
+                <a href=""><img src="../images/kari-shea-109894-unsplash%20copy.jpg"></a>
+                <div class="text-block">
+                    <h4>Article</h4>
+                    <p>Poop</p>
+                </div>
+            </div>
             </div>
         </div>
     </section>
